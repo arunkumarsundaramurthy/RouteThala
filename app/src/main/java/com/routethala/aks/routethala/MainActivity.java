5cb7f892b462906b88b9096d4491e7080d216a17
Arun Kumar Sundaramurthy <arunkumarsundaramurthy@gmail.com>
2017-08-20 11:03:55 +0530

    choose image from gallery

--- app/src/main/java/com/routethala/aks/routethala/MainActivity.java
+++ app/src/main/java/com/routethala/aks/routethala/MainActivity.java
@@ -1,13 +1,12 @@
 package com.routethala.aks.routethala;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -17,6 +16,7 @@ import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.UiThread;
 
+import java.io.IOException;
 import java.util.List;
 
 import butterknife.BindView;
@@ -27,6 +27,7 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_GALLERY_IMAGE = 2;
 
     private BusService service;
 
@@ -46,6 +47,33 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_ask_thala)
     public void askThala(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder
+                .setMessage(R.string.dialog_select_prompt)
+                .setPositiveButton(R.string.dialog_select_gallery, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startGalleryChooser();
+                    }
+                })
+                .setNegativeButton(R.string.dialog_select_camera, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startCamera();
+                    }
+                });
+        builder.create().show();
+    }
+
+    private void startGalleryChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select a photo"),
+                REQUEST_GALLERY_IMAGE);
+    }
+
+    private void startCamera() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -54,9 +82,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Bitmap imageBitmap = null;
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageBitmap = (Bitmap) extras.get("data");
+        } else if (requestCode == REQUEST_GALLERY_IMAGE && resultCode == RESULT_OK && data != null) {
+            try {
+                imageBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(imageBitmap != null) {
             tagBus(imageBitmap);
             mImageView.setImageBitmap(imageBitmap);
         }
@@ -72,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
         if(busResultList.size() > 0) {
             String message = "Correct bus: ";
             for(int i=0; i < busResultList.size(); i++) {
-                message += busResultList.get(i).getRouteNo() + " " + busResultList.get(i).isCorrectBus();
+                message += busResultList.get(i).getRouteNo();
             }
             Toast.makeText(this, message, Toast.LENGTH_LONG).show();
         } else {
